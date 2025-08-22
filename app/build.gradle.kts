b9749756e41e8059c0aa9f8d53ef174054111ac8
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2025-08-23 01:08:59 +0300

    updated version info to v0.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "v0.0.0"
+        versionName = "v0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
