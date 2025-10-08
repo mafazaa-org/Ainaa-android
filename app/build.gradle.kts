23875e2b488bdc220665b0b5cdcd7222ef670948
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2025-10-08 23:31:03 +0300

    updated version info to v0.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.mafazaa.ainaa"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "v0.0.5"
+        versionCode = 12
+        versionName = "v0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
